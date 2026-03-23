c518d056433d1b52feba9f76a0d352f346745c05
Zurol <zurolk@gmail.com>
2026-03-23 16:48:16 -0600

    feat: git pages.

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    // Reemplaza 'nombre-del-repo' por el nombre exacto de tu repositorio en GitHub
+    base: '/watermarkmodel/',
+})
